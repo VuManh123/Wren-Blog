4655c02c40aa4111c5aca9f18eb0ef9e067c4ab4
VuManh <vuducmanh10a@gmail.com>
2025-01-01 20:52:58 +0700

    deploy admin

--- admin/src/environments/environment.ts
+++ admin/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000' // URL của backend API
+    apiUrl: 'https://wren-blog.onrender.com' // URL của backend API
   };
   
